bf9a28b35a35a2207b8fe81243cfc414afb072e6
neekunjchaturvedi <neekunjchaturvedi3@gmail.com>
2025-12-23 23:11:29 +0530

    feat: mailService/RabbitMQ/grpc

---
+++ notification/src/utils/templates/types.ts
@@ -0,0 +1,13 @@
+export type EmailTemplateType = "OTP" | "WELCOME" | "RESET_PASSWORD";
+
+export type TemplateMap = {
+  OTP: {
+    name?: string;
+    otp: string;
+    expiryMinutes?: number;
+  };
+  WELCOME: {
+    name: string;
+  };
+  RESET_PASSWORD: { link: string };
+};
